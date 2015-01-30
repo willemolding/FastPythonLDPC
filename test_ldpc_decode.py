6ac66fba2f59ce447dee6b5916f834d86d3e5124
willemolding <544oxwou>
2015-01-30 17:44:33 +1100

    adds new cython file

--- test_ldpc_decode.py
+++ test_ldpc_decode.py
@@ -8,11 +8,11 @@ from ldpc_decode import ldpc_decode
 def run_test():
 
     data = io.loadmat('test_code.mat')
-    G = data['G']
     H = data['H']
     x = data['x']
     y = data['y']
     z = data['z']
+    size_G = data['size_G'][0]
     sigma = data['sigma']
 
     f1 = 1. / (1 + np.exp(-2 * z / sigma ** 2))  # likelihoods
@@ -25,10 +25,11 @@ def run_test():
         z_hat, success, k = ldpc_decode(f0, f1, H, 100)
     t1 = time.time()
 
-    x_hat = z_hat[G.shape[1] - G.shape[0]:G.shape[1]]
+    x_hat = z_hat[size_G[1] - size_G[0]:size_G[1]]
     b = x_hat.T
 
     nErrors = np.sum(x != b)
+    print 'decoding', z_hat.shape[0], 'code bits'
     print "n_errors", nErrors
     print "completed in ", t1-t0, "seconds"
     print 'iterations', k
